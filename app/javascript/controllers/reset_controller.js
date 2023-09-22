cff303fc37a72a5a4c9c85dc8bd9fa88abe3b598
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-09-22 17:59:48 -0500

    Form and stimulus JS Front-End

--- app/javascript/controllers/reset_controller.js
+++ app/javascript/controllers/reset_controller.js
@@ -2,6 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   resetForm() {
-    document.getElementById('modal').remove()
+    document.getElementById('modal').remove();
+  }
+
+  questionForm() {
+    const timer = setInterval(() => {
+      document.getElementById('new_question').reset();
+      clearInterval(timer);
+    }, 100)
   }
 }
