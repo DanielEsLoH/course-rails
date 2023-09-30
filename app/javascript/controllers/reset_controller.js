f28c08c443748b83e876b4e57e0f26057cd5b23a
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-09-29 19:20:27 -0500

    Stimulus JS for Option's form.

--- app/javascript/controllers/reset_controller.js
+++ app/javascript/controllers/reset_controller.js
@@ -1,4 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
+const restoreForm = (id) => {
+  document.getElementById(`${id}`).reset();
+}
 
 export default class extends Controller {
   resetForm() {
@@ -6,8 +9,17 @@ export default class extends Controller {
   }
 
   questionForm() {
+    const formId = "new_question";
     const timer = setInterval(() => {
-      document.getElementById('new_question').reset();
+      restoreForm(formId)
+      clearInterval(timer);
+    }, 100)
+  }
+
+  optionForm(e){
+    const formId = e.target.id;
+    const timer = setInterval(() => {
+      restoreForm(formId)
       clearInterval(timer);
     }, 100)
   }
