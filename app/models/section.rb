4bd01ccfe18da2386bdd0ea9c14bc70709be77c8
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-10-03 20:15:45 -0500

    SurveySections partials

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,8 +1,13 @@
 class Section < ApplicationRecord
   scope :grab_all_evaluations, -> { where ("section_type = 1")}
   scope :grab_all_chapters, -> { where ("section_type = 2")}
+
   belongs_to :user
   has_many :questions, dependent: :destroy
+
+  has_many :survey_sections
+  has_many :surveys, through: :survey_sections
+
   has_rich_text :body
   enum section_type: [:default, :evaluation, :chapter]
 end
