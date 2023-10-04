e84c7b64f3e323c4991a0a4877560416fbdb4809
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-10-03 21:14:44 -0500

    Add sections view

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,30 @@
 Rails.application.routes.draw do
-  resources :surveys
+  resources :surveys do
+    get "add/sections", to: "survey_sections#add_sections"
+    post "save/sections", to: "survey_sections#save_section"
+  end
+
   resources :questions do
     post "option/create", to: "options#create"
     get 'option/edit', to: 'options#edit'
     patch 'option/update', to: 'options#update'
     delete 'option/destroy', to: 'options#destroy'
   end
+
   resources :sections do
     post 'question/create', to: 'questions#create'
     get 'question/edit', to: 'questions#edit'
     patch 'question/update', to: 'questions#update'
     delete 'question/destroy', to: 'questions#destroy'
   end
+
   resources :chapters do
     get 'body/edit', to: "chapters#body"
   end
+
   resources :evaluations
   devise_for :users
+
   authenticated(:user) do
     root 'pages#index', as: :authenticated_root
   end
