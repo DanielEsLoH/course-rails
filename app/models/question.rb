1b63da38f2d97faefed466b9f0bfb4683c312503
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-10-11 16:37:18 -0500

    CSV method

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :section
   has_many :options, dependent: :destroy
+  has_one :answer, dependent: :destroy
   enum question_type: [:default, :text_free, :multiple, :level]
 end
