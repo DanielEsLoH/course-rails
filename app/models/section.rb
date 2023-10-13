11b8386fb667bf62f67ac51095ced8373d87251f
Daniel Lonodoño <daniel.esloh@gmail.com>
2023-10-12 22:04:40 -0500

    Fix bugs

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,12 +1,12 @@
 class Section < ApplicationRecord
-  scope :grab_all_evaluations, -> { where ("section_type = 1")}
-  scope :grab_all_chapters, -> { where ("section_type = 2")}
+  scope :grab_all_evaluations, -> { where("section_type = 1") }
+  scope :grab_all_chapters, -> { where("section_type = 2") }
 
   belongs_to :user
   has_many :questions, dependent: :destroy
 
-  has_many :survey_sections
-  has_many :surveys, through: :survey_sections
+  has_many :survey_sections, dependent: :destroy
+  has_many :surveys, through: :survey_sections, dependent: :destroy
 
   has_rich_text :body
 
